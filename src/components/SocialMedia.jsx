578ef39f0fc8ee87bdbaca6934d438abf53939e8
juan <juanmondragon206@gmail.com>
2022-11-17 13:22:00 -0800

    update

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+
+import {AiFillGithub} from 'react-icons/ai'
+import{AiFillLinkedin} from 'react-icons/ai'
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <AiFillGithub />
     </div>
     <div>
-      <FaFacebookF />
+      <AiFillLinkedin />
     </div>
-    <div>
-      <BsInstagram />
-    </div>
+    
   </div>
 );
 
